5a3770b3409f03c87e5c7ad0256cd938584b949b
Jayadeepsai <jayadeepsaim@gmail.com>
2022-10-13 10:06:27 +0530

    changed port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./app/app')
 // Create an instance of the http server to handle HTTP requests
-const port =process.env.PORT || 8000;
+const port =process.env.PORT || 5000;
 
 const server = http.createServer(app);
 // Start the server on port 8000
 server.listen(port);
-console.log('Node server running on port 8000');
+
+console.log('Node server running on port',port);
