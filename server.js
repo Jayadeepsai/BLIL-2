dba5694d9ecc1d402abcb13f90b975fdfbb1fbb8
Jayadeepsai <jayadeepsaim@gmail.com>
2022-10-14 09:48:37 +0530

    rrr

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('c:/Users/jayad/Desktop/BLIL/app/app.js')
 // Create an instance of the http server to handle HTTP requests
-const port =process.env.PORT || 5000;
+const port = 5000;
 
 const server = http.createServer(app);
 // Start the server on port 8000
